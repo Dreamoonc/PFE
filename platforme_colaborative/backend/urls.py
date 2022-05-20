0742a1b66d061ed1fff229c25dba888f33a1f923
amine <aminerate16@gmail.com>
2022-05-20 15:37:30 +0100

    auth pas complete

--- platforme_colaborative/backend/urls.py
+++ platforme_colaborative/backend/urls.py
@@ -3,5 +3,7 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('',views.Profile,name='login'),
+    path('',views.Profile,name='profile'),
+    path('login/',views.Login,name='login'),
+    path('signup/',views.Register,name='signup'),
 ]
